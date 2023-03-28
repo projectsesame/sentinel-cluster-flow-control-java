1fde3ea0da9519b31f3ac07bdde3bf5ff5472bc6
heng.jia <garroshh.jia@gmail.com>
2023-03-28 11:58:20 +0800

    add nacos config and k8s yaml

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM docker.m.daocloud.io/openjdk:8-alpine3.9
 
 WORKDIR /app
+ENV JAVA_OPTS ''
 COPY target/sentinel-cluster-flow-control-java-0.0.1-SNAPSHOT.jar .
-ENTRYPOINT java -jar sentinel-cluster-flow-control-java-0.0.1-SNAPSHOT.jar
+ENTRYPOINT java $JAVA_OPTS -jar sentinel-cluster-flow-control-java-0.0.1-SNAPSHOT.jar
--- src/main/java/skoala/io/daocloud/init/InitServer.java
+++ src/main/java/skoala/io/daocloud/init/InitServer.java
@@ -9,6 +9,7 @@ import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
 import com.alibaba.csp.sentinel.init.InitFunc;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRule;
+import com.alibaba.csp.sentinel.util.StringUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 
@@ -16,7 +17,6 @@ import java.util.List;
 import java.util.Set;
 
 public class InitServer implements InitFunc {
-    private static final String remoteAddress = "localhost:8848";
     private static final String groupId = "SENTINEL_GROUP";
     private static final String namespaceSetDataId = "cluster-server-namespace-set";
     private static final String serverTransportDataId = "cluster-server-transport-config";
@@ -26,25 +26,29 @@ public class InitServer implements InitFunc {
 
     @Override
     public void init() throws Exception {
-        System.out.println("-----------------init start--------------");
+        String nacosAddress = System.getProperty("NACOS_ADDRESS");
+        if (StringUtil.isBlank(nacosAddress)){
+            throw new RuntimeException("NACOS_ADDRESS env must be set");
+        }
+        System.out.printf("nacos address: %s\n", nacosAddress);
         ClusterFlowRuleManager.setPropertySupplier(namespace -> {
-            ReadableDataSource<String, List<FlowRule>> ds = new NacosDataSource<>(remoteAddress, groupId,
+            ReadableDataSource<String, List<FlowRule>> ds = new NacosDataSource<>(nacosAddress, groupId,
                     namespace + FLOW_POSTFIX,
                     source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {}));
             return ds.getProperty();
         });
         ClusterParamFlowRuleManager.setPropertySupplier(namespace -> {
-            ReadableDataSource<String, List<ParamFlowRule>> ds = new NacosDataSource<>(remoteAddress, groupId,
+            ReadableDataSource<String, List<ParamFlowRule>> ds = new NacosDataSource<>(nacosAddress, groupId,
                     namespace + PARAM_FLOW_POSTFIX,
                     source -> JSON.parseObject(source, new TypeReference<List<ParamFlowRule>>() {}));
             return ds.getProperty();
         });
 
-        ReadableDataSource<String, Set<String>> namespaceDs = new NacosDataSource<>(remoteAddress, groupId,
+        ReadableDataSource<String, Set<String>> namespaceDs = new NacosDataSource<>(nacosAddress, groupId,
                 namespaceSetDataId,
                 source -> JSON.parseObject(source, new TypeReference<Set<String>>() {}));
         ClusterServerConfigManager.registerNamespaceSetProperty(namespaceDs.getProperty());
-        ReadableDataSource<String, ServerTransportConfig> transportConfigDs = new NacosDataSource<>(remoteAddress, groupId,
+        ReadableDataSource<String, ServerTransportConfig> transportConfigDs = new NacosDataSource<>(nacosAddress, groupId,
                 serverTransportDataId,
                 source -> JSON.parseObject(source, new TypeReference<ServerTransportConfig>() {}));
         ClusterServerConfigManager.registerServerTransportProperty(transportConfigDs.getProperty());
